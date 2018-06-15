cd0e0f2f0c04f6c2a6cf1cb78b5c2b7b9e215df9
mladjan <mladjan.mihajlovickg@gmail.com>
2018-06-15 23:33:42 +0200

    Imlpemented most of emulator.

--- emulator/emulator.cpp
+++ emulator/emulator.cpp
@@ -12,6 +12,7 @@ void Emulator::startEmulation() {
 void Emulator::run() {
     while (running) {
         this->fetchInstruction();
+        this->getOperands();
     }
 }
 
@@ -155,85 +156,148 @@ void Emulator::getOperands() {
         switch (opCode) {
             case PUSH:
             case CALL: {
-                AddressingCode addressing = (AddressingCode)((cpu.ir0 & OP2_ADDR) >> OP2_ADDR_SHIFT);
-                switch (addressing) {
-                    case REGDIR: {
-                        char reg = (cpu.ir0 & OP2_REG);
-                        cpu.src = cpu.r[reg];
-                        break;
-                    }
+                char add = (cpu.ir0 & OP2_ADDR) >> OP2_ADDR_SHIFT;
+                AddressingCode addressing = (AddressingCode)add;
 
-                    case MEMDIR: {
-                        break;
-                    }
-
-                    case REGINDPOM: {
-                        break;
-                    }
-
-                    case IMMED: {
-                        char psw = (cpu.ir0 & OP2_REG);
-                        if ((psw == 0x7) && (opCode == CALL)) {
-                            throw AssemblingException("Cannot use psw register with call instruciton.");
-                        }
-
-                        short val = this->swapBytes(cpu.ir1);
-                        cpu.src = val;
-                    }
+                if (!this->addressingValid(addressing)) {
+                    throw EmulatingException("Invalid addressing code, addressingCode = " + addressing);
                 }
+
+                this->fetchOperand(cpu.src, OP2_ADDR, OP2_ADDR_SHIFT, OP2_REG, OP2_REG_SHIFT, opCode, addressing);
+                break;
             }
+
             default: {
-                // char add = (firstHalf & OP1_ADDR) >> OP1_ADDR_SHIFT;
-                // AddressingCode addressing = (AddressingCode)add;
+                char add = (cpu.ir0 & OP1_ADDR) >> OP1_ADDR_SHIFT;
+                AddressingCode addressing = (AddressingCode)add;
 
-                // if (!this->addressingValid(addressing)) {
-                //     throw EmulatingException("Invalid addressing code, addressingCode = " + addressing);
-                // }
+                if (!this->addressingValid(addressing)) {
+                    throw EmulatingException("Invalid addressing code, addressingCode = " + addressing);
+                }
 
-                // if (addressing != REGDIR) {
-                //     Address secondHalf = this->memory[cpu.r[PC]];
-                //     cpu.ir1 = secondHalf;                    
-                // }
+                this->fetchOperand(cpu.dst, OP1_ADDR, OP1_ADDR_SHIFT, OP1_REG, OP1_REG_SHIFT, opCode, addressing);
+
                 return;
             }
         }
     }
     else {
-        // char add1 = (firstHalf & OP1_ADDR) >> OP1_ADDR_SHIFT;
-        // AddressingCode addressing1 = (AddressingCode)add1;
+        //Getting first operand
+        char add1 = (cpu.ir0 & OP1_ADDR) >> OP1_ADDR_SHIFT;
+        AddressingCode addressing1 = (AddressingCode)add1;
 
-        // if (!this->addressingValid(addressing1)) {
-        //     throw EmulatingException("Invalid addressing code, addressingCode = " + addressing1);
-        // }
+        if (!this->addressingValid(addressing1)) {
+            throw EmulatingException("Invalid addressing code, addressingCode = " + addressing1);
+        }
 
-        // bool hasSecond = false;
-        // if (addressing1 != REGDIR) {
-        //     Address secondHalf = this->memory[cpu.r[PC]];
-        //     cpu.ir1 = secondHalf;
-        //     hasSecond = true;                    
-        // }
-        
-        // char add2 = (firstHalf & OP2_ADDR) >> OP2_ADDR_SHIFT;
-        // AddressingCode addressing2 = (AddressingCode)add2;
+        this->fetchOperand(cpu.dst, OP1_ADDR, OP1_ADDR_SHIFT, OP1_REG, OP1_REG_SHIFT, opCode, addressing1);
 
-        // if (!this->addressingValid(addressing2)) {
-        //     throw EmulatingException("Invalid addressing code, addressingCode = " + addressing2);
-        // }
+        //Getting second operand
+        char add2 = (cpu.ir0 & OP2_ADDR) >> OP2_ADDR_SHIFT;
+        AddressingCode addressing2 = (AddressingCode)add2;
 
-        // if (addressing2 != REGDIR) {
-        //     if (hasSecond) {
-        //         throw EmulatingException("Found combination of two memory addresing in one instruction.");
-        //     }
-        //     Address secondHalf = this->memory[cpu.r[PC]];
-        //     cpu.ir1 = secondHalf;
-        //     hasSecond = true;                    
-        // }
+        if (!this->addressingValid(addressing2)) {
+            throw EmulatingException("Invalid addressing code, addressingCode = " + addressing2);
+        }
 
-        // return;
+        this->fetchOperand(cpu.src, OP2_ADDR, OP2_ADDR_SHIFT, OP2_REG, OP2_REG_SHIFT, opCode, addressing2);
+
+
+        return;
     }
     
 }
 
+void Emulator::executeInstruction() {
+    InstructionCode opCode = (InstructionCode)((cpu.ir0 & OPCODE_MASK) >> OPCODE_SHIFT);
+
+    switch(opCode) {
+        case ADD: 
+        case SUB: 
+        case MUL: 
+        case DIV: {
+            this->doArithmeticInstruction(opCode);
+            break;
+        }
+        case CMP: {
+            bool srcSign = (cpu.src & MOST_SIGNIFICANT_BIT);
+            bool dstSign = (cpu.dst & MOST_SIGNIFICANT_BIT);
+            
+            short temp = cpu.dst - cpu.src;
+            bool resSign = (temp & MOST_SIGNIFICANT_BIT);
+            
+            bool overflow = (!dstSign && srcSign && resSign) || (dstSign && !srcSign && !resSign);
+            if (overflow) {
+                cpu.psw = cpu.psw | SET_O;
+            }
+            else {
+                cpu.psw = cpu.psw & RESET_O;
+            }
+
+            bool carry = (!dstSign && srcSign && !resSign) || (dstSign && srcSign && resSign) || (dstSign && !srcSign && resSign);
+            if (carry) {
+                cpu.psw = cpu.psw |  SET_C;
+            }
+            else {
+                cpu.psw = cpu.psw & RESET_C;
+            }
+            break;
+        }
+        case AND:
+        case OR:
+        case NOT:
+        case TEST: {
+            this->doLogicInstruction(opCode);
+            break;
+        }
+        case PUSH: {
+            cpu.r[SP] -= 2;
+            if (cpu.r[SP] < this->stackStart - this->stackSize) {
+                throw EmulatingException("Stack overflow.");
+            }
+
+            this->setMemoryValue(this->memory + cpu.r[SP], cpu.src);
+            break;
+        }
+        case POP: {
+            if (cpu.r[SP] > this->stackStart) {
+                throw EmulatingException("Memory access violation.");
+            }
+            cpu.dst = this->getMemoryValue(this->memory + cpu.r[SP]);
+            cpu.r[SP] += 2;
+            break;
+        }
+        case CALL: {
+            break;
+        }
+        case IRET: {
+            if (cpu.r[SP] > this->stackStart) {
+                throw EmulatingException("Memory access violation.");
+            }
+            cpu.psw = this->getMemoryValue(this->memory + cpu.r[SP]);
+            cpu.r[SP] += 2;
+
+            if (cpu.r[SP] > this->stackStart) {
+                throw EmulatingException("Memory access violation.");
+            }
+
+            cpu.r[PC] = this->getMemoryValue(this->memory + cpu.r[SP]);
+            cpu.r[SP] += 2;
+            break;
+        }
+        case MOV: {
+            cpu.dst = cpu.src;
+            this->setZN();
+            break;
+        }
+        case SHL: {
+            break;
+        }
+        case SHR: {
+            break;
+        }
+    }
+}
 bool Emulator::opCodeValid(const InstructionCode opCode) const {
     return (opCode >= InstructionCode::ADD) && (opCode <= InstructionCode::SHR);
 }
@@ -257,3 +321,228 @@ Address Emulator::getMemoryValue(char* addr) const {
     
     return *mar;
 }
+
+void Emulator::fetchOperand(short& writeReg, short opAddr, short opAddrShift, short opReg, short opRegShift, InstructionCode opCode, AddressingCode addressing) {
+
+    switch (addressing) {
+        case REGDIR: {
+            char reg = 0;
+            if (opRegShift) {
+                reg = (cpu.ir0 & opReg) >> opRegShift;
+            }
+            else {
+                reg = cpu.ir0 & opReg;
+            }
+            writeReg = cpu.r[reg];
+            break;
+        }
+
+        case MEMDIR: {
+            Address val = this->getMemoryValue(this->memory + cpu.ir1);
+            writeReg = val;
+            break;
+        }
+
+        case REGINDPOM: {
+            char reg = 0;
+            if (opRegShift) {
+                reg = (cpu.ir0 & opReg) >> opRegShift;
+            }
+            else {
+                reg = cpu.ir0 & opReg;
+            }
+            Address val = this->getMemoryValue(this->memory + cpu.ir1 + cpu.r[reg]);
+            writeReg = val;
+            break;
+        }
+
+        case IMMED: {
+            //checking if argument is psw
+            bool psw = false;
+            if (opRegShift) {
+                psw = ((cpu.ir0 & opReg) >> opRegShift) == 0x7;
+            }
+            else {
+                psw = (cpu.ir0 & opReg) == 0x7;
+            }
+
+            if (psw  && (opCode == CALL)) {
+                throw AssemblingException("Cannot use psw register with call instruciton.");
+            }
+
+            else if (psw) {
+                writeReg = cpu.psw;
+            }
+
+            else {
+                writeReg = cpu.ir1;
+            }
+            break;
+        }
+    }
+}
+
+void Emulator::doLogicInstruction(InstructionCode opCode) {
+    switch(opCode) {
+        case AND: {
+            cpu.dst &= cpu.src;
+            break;
+        }
+        case OR: {
+            cpu.dst |= cpu.src;
+            break;
+        }
+        case NOT: {
+            cpu.dst = ~cpu.src;
+            break;
+        }
+        case TEST: {
+            Address temp = cpu.dst & cpu.src;
+            
+            if (temp == 0) {
+                cpu.psw = cpu.psw | SET_Z;
+            }
+            else {
+                cpu.psw = cpu.psw & RESET_Z;
+            }
+
+            if (temp & MOST_SIGNIFICANT_BIT) {
+                cpu.psw = cpu.psw | SET_N;
+            }
+            else {
+                cpu.psw = cpu.psw & RESET_N;
+            }
+
+            break;
+        }
+        default: {
+            this->invalidInstruciton();
+            return;
+        }
+        
+        if (opCode != TEST) {
+            this->setZN();
+        } 
+    }
+}
+
+void Emulator::doArithmeticInstruction(InstructionCode opCode) {
+    
+    bool srcSign = (cpu.src & MOST_SIGNIFICANT_BIT);
+    bool dstSign = (cpu.dst & MOST_SIGNIFICANT_BIT);
+
+    switch(opCode) {
+        case ADD: {
+            cpu.dst += cpu.src;
+            break;
+        }           
+        case SUB: {
+            cpu.dst -= cpu.src;
+            break;
+        }
+        case MUL: {
+            cpu.dst *= cpu.src;
+            break;
+        }
+        case DIV: {
+            cpu.dst /= cpu.src;
+            break;
+        }
+    }
+
+    bool resSign = cpu.dst & MOST_SIGNIFICANT_BIT;
+
+    this->setZN();
+    if (opCode == ADD) {
+        bool overflow = (dstSign && srcSign && !resSign) || (!dstSign && !srcSign && resSign);
+        if (overflow) {
+            cpu.psw = cpu.psw | SET_O;
+        }
+        else {
+            cpu.psw = cpu.psw & RESET_O;
+        }
+
+        bool carry = (dstSign && srcSign) || (!dstSign && srcSign && !resSign) || (dstSign && !srcSign && !resSign);
+        if (carry) {
+            cpu.psw = cpu.psw |  SET_C;
+        }
+        else {
+            cpu.psw = cpu.psw & RESET_C;
+        }
+        
+    }
+    else if(opCode == SUB) {
+        bool overflow = (!dstSign && srcSign && resSign) || (dstSign && !srcSign && !resSign);
+        if (overflow) {
+            cpu.psw = cpu.psw | SET_O;
+        }
+        else {
+            cpu.psw = cpu.psw & RESET_O;
+        }
+
+        bool carry = (!dstSign && srcSign && !resSign) || (dstSign && srcSign && resSign) || (dstSign && !srcSign && resSign);
+        if (carry) {
+            cpu.psw = cpu.psw |  SET_C;
+        }
+        else {
+            cpu.psw = cpu.psw & RESET_C;
+        }
+    }
+}
+
+void Emulator::doShift(InstructionCode opCode) {
+    if (opCode == SHL) {
+        bool carry = 
+    }
+}
+
+void Emulator::invalidInstruciton() {
+
+}
+
+void Emulator::setZN() {
+    if (cpu.dst == 0) {
+        cpu.psw = cpu.psw | SET_Z;
+    }
+    else {
+        cpu.psw = cpu.psw & RESET_Z;
+    }
+    if (cpu.dst & MOST_SIGNIFICANT_BIT) {
+        cpu.psw = cpu.psw | SET_N;
+    }
+    else {
+        cpu.psw = cpu.psw & RESET_N;
+    }
+}
+
+
+
+
+// ADD
+//        A                   B                   A + B              Flags  
+//  ---------------     ----------------    ---------------      -----------------
+//  h  |  ud  |   d   | h  |  ud  |   d   | h  |  ud  |   d   | OF | SF | ZF | CF
+//  ---+------+-------+----+------+-------+----+------+-------+----+----+----+---
+//  7F | 127  |  127  | 0  |  0   |   0   | 7F | 127  |  127  | 0  | 0  | 0  | 0
+//  FF | 255  |  -1   | 7F | 127  |  127  | 7E | 126  |  126  | 0  | 0  | 0  | 1
+//  0  |  0   |   0   | 0  |  0   |   0   | 0  |  0   |   0   | 0  | 0  | 1  | 0
+//  FF | 255  |  -1   | 1  |  1   |   1   | 0  |  0   |   0   | 0  | 0  | 1  | 1
+//  FF | 255  |  -1   | 0  |  0   |   0   | FF | 255  |  -1   | 0  | 1  | 0  | 0
+//  FF | 255  |  -1   | FF | 255  |  -1   | FE | 254  |  -2   | 0  | 1  | 0  | 1
+//  FF | 255  |  -1   | 80 | 128  | -128  | 7F | 127  |  127  | 1  | 0  | 0  | 1
+//  80 | 128  | -128  | 80 | 128  | -128  | 0  |  0   |   0   | 1  | 0  | 1  | 1
+//  7F | 127  |  127  | 7F | 127  |  127  | FE | 254  |  -2   | 1  | 1  | 0  | 0
+
+
+//  SUB
+//        A                   B                   A - B              Flags  
+//  ---------------     ----------------    ---------------      -----------------
+//  h  |  ud  |   d   | h  |  ud  |   d   | h  |  ud  |   d   || OF | SF | ZF | CF
+// ----+------+-------+----+------+-------+----+------+-------++----+----+----+----
+//  FF | 255  |  -1   | FE | 254  |  -2   | 1  |  1   |   1   || 0  | 0  | 0  | 0
+//  7E | 126  |  126  | FF | 255  |  -1   | 7F | 127  |  127  || 0  | 0  | 0  | 1
+//  FF | 255  |  -1   | FF | 255  |  -1   | 0  |  0   |   0   || 0  | 0  | 1  | 0
+//  FF | 255  |  -1   | 7F | 127  |  127  | 80 | 128  | -128  || 0  | 1  | 0  | 0
+//  FE | 254  |  -2   | FF | 255  |  -1   | FF | 255  |  -1   || 0  | 1  | 0  | 1
+//  FE | 254  |  -2   | 7F | 127  |  127  | 7F | 127  |  127  || 1  | 0  | 0  | 0
+//  7F | 127  |  127  | FF | 255  |  -1   | 80 | 128  | -128  || 1  | 1  | 0  | 1
--- h/emulator.h
+++ h/emulator.h
@@ -6,39 +6,55 @@
 
 #define PC 7
 #define SP 8
+
 #define OPCODE_MASK 0x3C00
 #define OPCODE_SHIFT 10
+
 #define OP1_ADDR 0x0300
 #define OP1_ADDR_SHIFT 8
 #define OP1_REG 0x00E0
 #define OP1_REG_SHIFT 5
+
 #define OP2_ADDR 0x0018
 #define OP2_ADDR_SHIFT 3
 #define OP2_REG 0x0007
+#define OP2_REG_SHIFT 0x0
 
+#define RESET_Z 0xFFFE
+#define SET_Z 0x0001
+#define RESET_O 0xFFFD
+#define SET_O 0x0002
+#define RESET_C 0xFFFB
+#define SET_C 0x0004
+#define RESET_N 0xFFF7
+#define SET_N 0x0008
+
+#define NEGATIVE_MASK 0x8000
+#define MOST_SIGNIFICANT_BIT 0x8000
 namespace ss {
 typedef unsigned short Address;
     struct CPU {
-        short r[8];
-        short psw;
+        Address r[8];
+        Address psw;
         short src;
         short dst;
-        short alu;
-        short ir0;
-        short ir1;
+        Address alu;
+        Address ir0;
+        Address ir1;
     };
 
 
     class Emulator {
     public:
-        Emulator(Executable e) {
-            this->memory = e.content;
-            this->cpu.r[7] = e.startAddress;
+        Emulator(Executable e) : Emulator(e.content, e.startAddress) {
+            //this->memory = e.content;
+            //this->cpu.r[7] = e.startAddress;
         }
 
         Emulator(char* memory, Address start) : callStack(0), running(false) {
             this->cpu.r[7] = start;
             this->memory = memory;
+            bool instructionError = false;
         }
 
         void startEmulation();
@@ -50,15 +66,34 @@ typedef unsigned short Address;
         void getOperands();
         void executeInstruction();
 
+
+        void fetchOperand(short& writeReg, short opAddr, short opAddrShift, short opReg, short opRegShift, InstructionCode opCode, AddressingCode addresing);
+        void setZN();
+
         bool opCodeValid(const InstructionCode opCode) const;
         bool addressingValid(const AddressingCode addCode) const;
+
+        void doLogicInstruction(InstructionCode opCode);
+        void doArithmeticInstruction(InstructionCode opCode);
+        void doShift(InstructionCode opCode);
+        
+        void invalidInstruciton();
+
         Address swapBytes(const Address bytes) const;
+
         Address getMemoryValue(char* memoryLocation) const;
+        void setMemoryValue(char* memoryLocation, short& value);
 
         CPU cpu;
         char* memory;
+        
+        Address stackStart;
+        Address stackSize;
+
         int callStack;
         bool running;
+
+        bool instructionError;
     };
 }
 #endif
